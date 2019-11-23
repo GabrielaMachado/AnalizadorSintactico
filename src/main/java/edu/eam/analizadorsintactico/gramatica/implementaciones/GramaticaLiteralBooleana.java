d8a75bda698384c3c0b50b29981a7bcb7656724d
DanielaDiazP <dani092012@hotmail.com>
2019-11-23 02:31:58 -0500

    gramatica literalBooleana y While

--- src/main/java/edu/eam/analizadorsintactico/gramatica/implementaciones/GramaticaLiteralBooleana.java
+++ src/main/java/edu/eam/analizadorsintactico/gramatica/implementaciones/GramaticaLiteralBooleana.java
@@ -5,10 +5,40 @@
  */
 package edu.eam.analizadorsintactico.gramatica.implementaciones;
 
+import edu.eam.analizadorlexicos.Lexema;
+import edu.eam.analizadorlexicos.TipoLexemaEnum;
+import static edu.eam.analizadorsintactico.controlador.AnalizadorSintactico.posicion;
+import edu.eam.analizadorsintactico.gramatica.definiciones.Gramatica;
+import edu.eam.analizadorsintactico.sentencias.definicion.Sentencia;
+import edu.eam.analizadorsintactico.sentencias.implementaciones.LiteralBooleana;
+import edu.eam.analizadorsintactico.sentencias.implementaciones.TipoDatoNumerico;
+import java.util.ArrayList;
+
 /**
  *
  * @author dani0
  */
-public class GramaticaLiteralBooleana {
+public class GramaticaLiteralBooleana implements Gramatica{
     
+    @Override
+    public Sentencia analizar(ArrayList<Lexema> arrayLexemas) {
+//Sentencia a retornar....
+        LiteralBooleana literalBooleana = new LiteralBooleana();
+        int posI = posicion;
+        int posA = posicion;
+        //primer token de la gramatica.
+        Lexema lexema = arrayLexemas.get(posA);
+
+        //tipo de dato.....
+        if (lexema.getTipo() == TipoLexemaEnum.LBOL_TRUE || lexema.getTipo() == TipoLexemaEnum.LBOL_FALSE) {
+            literalBooleana.setLiteralBooleana(lexema);
+            posA++;
+            posicion = posA;
+            return literalBooleana;
+        } else {
+            posA = posI;
+            return null;
+        }
+    }
+
 }
