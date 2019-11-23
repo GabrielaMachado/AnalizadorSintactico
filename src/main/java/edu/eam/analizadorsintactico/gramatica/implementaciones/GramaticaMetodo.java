559f8790093662e5977be1e68b4fd58e6d1c9068
DanielaDiazP <dani092012@hotmail.com>
2019-11-22 22:41:58 -0500

    gramaticas parametro y metodo

--- src/main/java/edu/eam/analizadorsintactico/gramatica/implementaciones/GramaticaMetodo.java
+++ src/main/java/edu/eam/analizadorsintactico/gramatica/implementaciones/GramaticaMetodo.java
@@ -5,10 +5,113 @@
  */
 package edu.eam.analizadorsintactico.gramatica.implementaciones;
 
+import edu.eam.analizadorlexicos.Lexema;
+import edu.eam.analizadorlexicos.TipoLexemaEnum;
+import static edu.eam.analizadorsintactico.controlador.AnalizadorSintactico.posicion;
+import edu.eam.analizadorsintactico.gramatica.definiciones.Gramatica;
+import edu.eam.analizadorsintactico.sentencias.definicion.Sentencia;
+import edu.eam.analizadorsintactico.sentencias.implementaciones.Cuerpo;
+import edu.eam.analizadorsintactico.sentencias.implementaciones.Metodo;
+import edu.eam.analizadorsintactico.sentencias.implementaciones.Parametro;
+import edu.eam.analizadorsintactico.sentencias.implementaciones.TipoDato;
+import java.util.ArrayList;
+
 /**
  *
  * @author user
  */
-public class GramaticaMetodo {
-    
+public class GramaticaMetodo implements Gramatica {
+
+    @Override
+    public Sentencia analizar(ArrayList<Lexema> arrayLexemas) {
+        Metodo metodo = new Metodo();
+        TipoDato tipoDato;
+        Parametro parametro;
+        Cuerpo cuerpo;
+        Gramatica gramaticaTipoDato = new GramaticaTipoDato();
+        Gramatica gramaticaParametro = new GramaticaParametro();
+        Gramatica gramaticaCuerpo = new GramaticaCuerpo();
+
+        int posI = posicion;
+        int posA = posicion;
+        //primer token de la gramatica.
+        Lexema lexema = arrayLexemas.get(posA);
+
+        tipoDato = (TipoDato) gramaticaTipoDato.analizar(arrayLexemas);
+        //tipo de dato.....
+        if (tipoDato != null) {
+            metodo.setTipoDato(tipoDato);
+            posA++;
+            lexema = arrayLexemas.get(posA);
+        }
+        if (lexema.getTipo() == TipoLexemaEnum.IDENT) {
+            metodo.setIdent(lexema);
+            posA++;
+            lexema = arrayLexemas.get(posA);
+
+            if (lexema.getTipo() == TipoLexemaEnum.AGR_OPENP) {
+                posA++;
+                lexema = arrayLexemas.get(posA);
+                posicion = posA;
+                parametro = (Parametro) gramaticaParametro.analizar(arrayLexemas);
+
+                if (parametro != null) {
+                    metodo.setParametro(parametro);
+                    posA++;
+                    lexema = arrayLexemas.get(posA);
+
+                    if (lexema.getTipo() == TipoLexemaEnum.AGR_CLOSEP) {
+                        posA++;
+                        lexema = arrayLexemas.get(posA);
+
+                        if (lexema.getTipo() == TipoLexemaEnum.AGR_OPENKEY) {
+                            posA++;
+                            lexema = arrayLexemas.get(posA);
+                            posicion = posA;
+                            cuerpo = (Cuerpo) gramaticaCuerpo.analizar(arrayLexemas);
+
+                            if (cuerpo != null) {
+                                metodo.setCuerpo(cuerpo);
+                                posA++;
+                                lexema = arrayLexemas.get(posA);
+
+                                if (lexema.getTipo() == TipoLexemaEnum.AGR_CLOSEKEY) {
+                                    posA++;
+                                    lexema = arrayLexemas.get(posA);
+
+                                    if (lexema.getTipo() == TipoLexemaEnum.DELIMITADOR) {
+                                        posicion = posA;
+                                        return metodo;
+                                    } else {
+                                        posA = posI;
+                                        return null;
+                                    }
+                                } else {
+                                    posA = posI;
+                                    return null;
+                                }
+                            } else {
+                                posA = posI;
+                                return null;
+                            }
+                        } else {
+                            posA = posI;
+                            return null;
+                        }
+                    } else {
+                        posA = posI;
+                        return null;
+                    }
+                } else {
+                    posA = posI;
+                    return null;
+                }
+            } else {
+                posA = posI;
+                return null;
+            }
+        }
+        return null; //se retorna null para que se pruebe con otra regal..
+    }
+
 }
--- src/main/java/edu/eam/analizadorsintactico/sentencias/implementaciones/Metodo.java
+++ src/main/java/edu/eam/analizadorsintactico/sentencias/implementaciones/Metodo.java
@@ -5,10 +5,98 @@
  */
 package edu.eam.analizadorsintactico.sentencias.implementaciones;
 
+import edu.eam.analizadorlexicos.Lexema;
+import edu.eam.analizadorsintactico.sentencias.definicion.Sentencia;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author user
  */
-public class Metodo {
+public class Metodo extends Sentencia{
     
+    /**
+     * Modificador de acceso del metodo.
+     */
+    private TipoDato tipoDato;
+    private Lexema ident;
+    private Parametro parametro;
+    private Cuerpo cuerpo;
+
+    public Metodo() {
+    }
+
+    public Metodo(TipoDato tipoDato, Lexema ident, Parametro parametro, Cuerpo cuerpo) {
+        this.tipoDato = tipoDato;
+        this.ident = ident;
+        this.parametro = parametro;
+        this.cuerpo = cuerpo;
+    }
+
+    @Override
+    public List<Sentencia> llenarHijos() {
+        hijos = new ArrayList<>();
+
+        if (tipoDato != null) {
+            hijos.add(new SentenciaLexema(ident));
+        }
+        if (ident != null) {
+            hijos.add(new SentenciaLexema(ident));
+        }
+        if (parametro != null) {
+            hijos.add(parametro);
+        }
+        if (cuerpo != null) {
+            hijos.add(cuerpo);
+        }
+        for (int i = 0; i < hijos.size(); i++) {
+            System.out.println("hijo " + i + "  " + hijos.get(i));
+        }
+        return hijos;
+
+    }
+
+    public TipoDato getTipoDato() {
+        return tipoDato;
+    }
+
+    public void setTipoDato(TipoDato tipoDato) {
+        this.tipoDato = tipoDato;
+    }
+
+    public Lexema getIdent() {
+        return ident;
+    }
+
+    public void setIdent(Lexema ident) {
+        this.ident = ident;
+    }
+
+    public Parametro getParametro() {
+        return parametro;
+    }
+
+    public void setParametro(Parametro parametro) {
+        this.parametro = parametro;
+    }
+
+    public Cuerpo getCuerpo() {
+        return cuerpo;
+    }
+
+    public void setCuerpo(Cuerpo cuerpo) {
+        this.cuerpo = cuerpo;
+    }
+
+    @Override
+    public String toString() {
+        return "Metodo :" + ident.getToken();
+    }
+
+    @Override
+    public String parse() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
